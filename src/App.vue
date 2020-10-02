c5b7fedced637839feaf7cb63a51a54ca3673e0f
新-岸 <1400330380@qq.com>
2020-10-02 08:40:04 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+<img alt="Vue logo" src="./assets/logo.png" />
+<HelloWorld msg="Hello Vue 3.0 + Vite" />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Hi from './components/hi.vue'
+
+export default {
+    name: 'App',
+    components: {
+        HelloWorld: Hi
+    }
+}
+</script>
